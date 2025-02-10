d6814b5cf853be51d8462a4d890753023637106f
Tushar-Verma <tusharmyself06@gmail.com>
2025-02-10 07:57:09 +0530

    Time: 675 ms (15.29%), Space: 12.5 MB (46%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -2,8 +2,8 @@ class Solution(object):
     def fib(self, n):
         if n==0 or n==1:
             return n
-        last=self.fib(n-1)
-        slast=self.fib(n-2)
-        ans=last+slast
+        # last=self.fib(n-1)
+        # slast=self.fib(n-2)
+        ans=self.fib(n-1)+self.fib(n-2)
         return ans
         
