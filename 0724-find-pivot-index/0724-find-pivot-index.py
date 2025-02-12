696f6d23c8f57845f427ded7f15055f59e8d7a54
Tushar-Verma <tusharmyself06@gmail.com>
2025-02-12 09:59:03 +0530

    Time: 3174 ms (5.76%), Space: 13.5 MB (16.86%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,7 +1,10 @@
 class Solution(object):
     def pivotIndex(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        n=len(nums)
+        for i in range(n):
+            sum1=sum(nums[0:i])
+            sum2=sum(nums[i+1:n+1])
+            if sum1==sum2:
+                return i
+        return -1  
         
