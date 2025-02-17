180638fd5515d2ff0462f95a0cccd069eec50452
Tushar-Verma <tusharmyself06@gmail.com>
2025-02-17 08:51:11 +0530

    Time: 3 ms (49.8%), Space: 12.6 MB (74.18%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        result=[]
+        def generate(open,close,current):
+            if len(current)==2*n:
+                result.append(current)
+                return
+            if (open < n):
+                generate(open+1,close,current+"(")
+            if (close<open):
+                generate(open,close+1,current+")")
+        generate(0,0,"")
+        return result
+
+
+
+                
+
+        
