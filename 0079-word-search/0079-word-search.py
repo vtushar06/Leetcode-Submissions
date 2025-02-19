4bb4218afdd12faa05a9dd2176aea9c924b6220e
Tushar-Verma <tusharmyself06@gmail.com>
2025-02-19 07:34:10 +0530

    Time: 6548 ms (68.19%), Space: 12.4 MB (52.61%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,27 +1,24 @@
 class Solution(object):
-    direction=[[1,0],[0,1],[-1,0],[0,-1]]
-    def find(self,i,j,index,board,word,m,n):
-        
-        if index==len(word):return True
-        if i<0 or j<0 or i>=m or j>=n or board[i][j]!=word[index]:return False
-        
-        temp=board[i][j]
-        board[i][j]="$"
-        for dic in Solution.direction:
-            new_i=i+dic[0]
-            new_j=j+dic[1]
-            if self.find(new_i,new_j,index+1,board,word,m,n):
-                return True
-        board[i][j]=temp
-        return False       
+    def wordsearch(self,m,n,i,j,board,word,index):
+        if index==len(word):
+            return True
+        if i<0 or j<0 or i>=m or j>=n  or board[i][j]!=word[index]:
+            return False
+        tempchar=board[i][j]
+        board[i][j]="#"
+        opt1=self.wordsearch(m,n,i+1,j,board,word,index+1) 
+        opt2=self.wordsearch(m,n,i,j+1,board,word,index+1)    
+        opt3=self.wordsearch(m,n,i-1,j,board,word,index+1)    
+        opt4=self.wordsearch(m,n,i,j-1,board,word,index+1) 
+        board[i][j]=tempchar     
+        return opt1 or opt2 or opt3 or opt4   
+
     def exist(self, board, word):
         m=len(board)
         n=len(board[0])
         for i in range(m):
             for j in range(n):
-                if board[i][j]==word[0] and self.find(i,j,0,board,word,m,n):
-                    return True
-        return False            
-
-       
-        
+                if board[i][j]==word[0]:
+                    if(self.wordsearch(m,n,i,j,board,word,0)):
+                        return True
+        return False
