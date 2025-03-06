fdbc23a2457f597c3979eb1e8ae6ed6022f2cea3
Tushar-Verma <tusharmyself06@gmail.com>
2025-03-06 20:49:26 +0530

    Time: 84 ms (5.36%), Space: 20.1 MB (94.94%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1] :
+                return True 
+        return False     
         
