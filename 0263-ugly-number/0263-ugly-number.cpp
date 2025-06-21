7b471a95d3f67366ce54493c317800672bf6bc6c
Tushar-Verma <tusharmyself06@gmail.com>
2025-06-21 21:34:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (10.08%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0) return false; // Ugly numbers are positive
+
+        // Keep dividing by 2, 3, and 5
+        while (n % 2 == 0) n /= 2;
+        while (n % 3 == 0) n /= 3;
+        while (n % 5 == 0) n /= 5;
+
+        return n == 1;
+    }
+};
