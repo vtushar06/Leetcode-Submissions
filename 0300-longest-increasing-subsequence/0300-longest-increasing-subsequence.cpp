736f865b3d13f3ebea94f964fa9a921307134932
Tushar-Verma <tusharmyself06@gmail.com>
2025-07-03 13:01:11 +0530

    Time: 539 ms (22.05%), Space: 296.5 MB (22.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+
+
+#define pb push_back
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define srt(x) x.begin(), x.end()
+#define rsrt(x) x.rbegin(), x.rend()
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n+1,1);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[j]+1,dp[i]);
-                }
-            }
+    vector<vector<int>> dp;
+
+    int find(int i, int previdx, int n, vector<int>& a) {
+        if (i >= n) return 0;
+
+        if (dp[i][previdx + 1] != -1) return dp[i][previdx + 1];
+
+        // Not take the current element
+        int ans = find(i + 1, previdx, n, a);
+
+        // Take the current element if it's increasing
+        if (previdx == -1 || a[i] > a[previdx]) {
+            ans = max(ans, 1 + find(i + 1, i, n, a));
         }
-        int max_val = *max_element(dp.begin(), dp.end());
-        return max_val;
-    
-         
-        
+
+        return dp[i][previdx + 1] = ans;
+    }
+
+    int lengthOfLIS(vector<int>& a) {
+        int n = a.size();
+        dp.assign(n, vector<int>(n + 1, -1));
+        return find(0, -1, n, a);
     }
 };
+
+
