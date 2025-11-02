8dfdd477b15268182ea1ffa50277fb2000810cbd
Tushar-Verma <tusharmyself06@gmail.com>
2025-11-02 15:53:51 +0530

    Time: 8 ms (22.81%), Space: 18.8 MB (44.62%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp=[[-1]*2 for i in range(len(prices)+2)]
+    
+        def solve(idx,canbuy):
+            if idx>=len(prices):
+                return 0
+            if dp[idx][canbuy]!=-1:
+                return dp[idx][canbuy]
+
+            if (canbuy):
+                # is index pa ma pocket sa de raha hu tuo minus kiys and solve function ko kha raha hu ki bhai
+                # jo aaga sa profit ho vo lekar aah ja
+                buy=-prices[idx]+solve(idx+1,0)
+                # agar mana buy nahi kiya tuo mana solve function ko bola bhai 
+                # aaga sa jitna bhi profit ha leakr aah ja
+                notbuy=solve(idx+1,1) 
+
+                profit=max(buy,notbuy)   
+            else:
+                sell=prices[idx]+solve(idx+2,1)
+
+                notsell=solve(idx+1,0)
+
+                profit=max(sell,notsell)
+
+            dp[idx][canbuy]= profit
+            return profit
+
+        return solve(0,1)
+        
