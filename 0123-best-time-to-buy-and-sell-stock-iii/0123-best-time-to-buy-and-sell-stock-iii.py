e1350ead532c2d5449475d451bf31d18584fcbbf
Tushar-Verma <tusharmyself06@gmail.com>
2025-11-02 08:49:01 +0530

    Time: 1950 ms (9.54%), Space: 85.3 MB (15.11%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(len(prices))]
+
+        def solve(idx,canbuy,cap):
+            if cap==0 or idx==len(prices):
+                return 0
+            if dp[idx][canbuy][cap]!=-1:
+                return dp[idx][canbuy][cap]    
+            if (canbuy):
+                buy=-prices[idx]+solve(idx+1,0,cap)
+                notbuy=solve(idx+1,1,cap)
+                profit=max(buy,notbuy)
+            else:
+                sell=+prices[idx]+solve(idx+1,1,cap-1)
+                notsell=solve(idx+1,0,cap)
+                profit=max(sell,notsell)
+            dp[idx][canbuy][cap] = profit
+            return dp[idx][canbuy][cap] 
+
+        return solve(0,1,2) 
+        
