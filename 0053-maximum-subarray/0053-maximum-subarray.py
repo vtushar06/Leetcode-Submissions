7bf5cd7db6d482c11bc5754e8ddb7f110db885f4
Tushar-Verma <tusharmyself06@gmail.com>
2025-11-04 22:58:11 +0530

    Time: 45 ms (84.47%), Space: 32 MB (66.35%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -12,27 +12,3 @@ class Solution:
             
             r+=1   
         return maxsum    
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # sum1=0
-        # maxsum=nums[0]
-        # for num in nums:
-        #     sum1+=num
-        #     maxsum=max(maxsum,sum1)
-        #     if sum1<0:
-        #         sum1=0
-        # return maxsum    
-        
