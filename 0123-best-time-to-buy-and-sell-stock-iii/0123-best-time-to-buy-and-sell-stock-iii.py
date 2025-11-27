51b7442294a9eb9a157039e331eca13f163e4419
Tushar-Verma <tusharmyself06@gmail.com>
2025-11-27 12:40:29 +0530

    Time: 1918 ms (10.64%), Space: 85.3 MB (14.93%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -6,7 +6,7 @@ class Solution:
             if cap==0 or idx==len(prices):
                 return 0
             if dp[idx][canbuy][cap]!=-1:
-                return dp[idx][canbuy][cap]    
+                return dp[idx][canbuy][cap]
             if (canbuy):
                 buy=-prices[idx]+solve(idx+1,0,cap)
                 notbuy=solve(idx+1,1,cap)
@@ -15,8 +15,9 @@ class Solution:
                 sell=+prices[idx]+solve(idx+1,1,cap-1)
                 notsell=solve(idx+1,0,cap)
                 profit=max(sell,notsell)
-            dp[idx][canbuy][cap] = profit
-            return dp[idx][canbuy][cap] 
+                
+            dp[idx][canbuy][cap]=profit
+            return profit
+        return solve(0,1,2)        
 
-        return solve(0,1,2) 
-        
+
