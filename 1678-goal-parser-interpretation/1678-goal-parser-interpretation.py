f03f013c39ea5423355e1d1c9747e323275bd43e
Tushar-Verma <tusharmyself06@gmail.com>
2025-12-30 23:37:00 +0530

    Time: 46 ms (8.02%), Space: 17.2 MB (89.31%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        result = []
+        i = 0
+        while i < len(command):
+            if command[i] == 'G':
+                result.append('G')
+                i += 1
+            elif command[i:i+2] == '()':
+                result.append('o')
+                i += 2
+            else:  # "(al)"
+                result.append('al')
+                i += 4
+    
+        return ''.join(result)
+        
