083e76fff38fb3666c71757e72c8fa8d63646cdb
Tushar-Verma <tusharmyself06@gmail.com>
2026-01-22 21:15:33 +0530

    Time: 4 ms (40.89%), Space: 20.6 MB (12.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         indexdic={}
         for i,num in enumerate(nums):
             remaining=target-num
             if remaining in indexdic.keys():
                 return[i,indexdic[remaining]]
             indexdic[num]=i
-        
